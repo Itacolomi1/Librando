8128ccb00f205d377b3b2a9dffe1b902facb551e
patrcastro <patrcastro@hotmail.com>
2021-05-25 22:05:40 -0300

    API de update sala

--- controllers/api/usuario.controller.js
+++ controllers/api/usuario.controller.js
@@ -1,37 +1,38 @@
 ﻿// Esse é o mesmo controller do Mongo Stack! Apenas fiz a tradução das mensagens
 
-var express = require('express');
-var router = express.Router();
-var userService = require('services/usuario.service');
+let express = require('express')
+var router = express.Router()
+var userService = require('services/usuario.service')
 
 // routes
-router.post('/authenticate', authenticateUser);
-router.post('/register', registerUser);
+router.post('/authenticate', authenticateUser)
+router.post('/register', registerUser)
 
-module.exports = router;
+module.exports = router
 
-function authenticateUser(req, res) {
-    userService.authenticate(req.body.email, req.body.senha)
-        .then(function (response) {
-            if (response) {
-                // authentication successful
-                res.send({ userId: response.userId, token: response.token });                
+function authenticateUser (req, res) {
+  userService.authenticate(req.body.email, req.body.senha)
+    .then(function (response) {
+      if (response) {
+        // authentication successful
+        localStorage.setItem('token', response.token)
+        res.send({ userId: response.userId, token: response.token })                
             } else {
-                // authentication failed
-                res.status(401).send('Usuário e/ou senha inválidos');
+        // authentication failed
+        res.status(401).send('Usuário e/ou senha inválidos')
             }
+    })
+    .catch(function (err) {
+      res.status(400).send(err)
         })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
 }
 
-function registerUser(req, res) {
-    userService.create(req.body)
-        .then(function (user) {
-            res.status(200).send(user);
+function registerUser (req, res) {
+  userService.create(req.body)
+    .then(function (user) {
+      res.status(200).send(user)
         })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
+    .catch(function (err) {
+      res.status(400).send(err)
+        })
 }
--- services/sala.service.js
+++ services/sala.service.js
@@ -1,113 +1,134 @@
-var Q = require('q');
-var lodash = require('lodash');
-var mongoDB = require('config/database.js');
-const ObjectID = mongoDB.ObjectID();
-mongoDB.connect();
+const Q = require('q')
+const lodash = require('lodash')
+const mongoDB = require('config/database.js')
+const ObjectID = mongoDB.ObjectID()
+mongoDB.connect()
 
+const service = {}
+service.create = create
+service.verifica_sala_ativa = verifica_sala_ativa
+service.valida_codigo = valida_codigo
+service.lista_salas = lista_salas
+service.update = update
 
-var service = {};
-service.create = create;
-service.verifica_sala_ativa = verifica_sala_ativa;
-service.valida_codigo = valida_codigo;
-service.lista_salas = lista_salas;
+module.exports = service
 
-module.exports = service;
+function create (salaParam) {
+  const deferred = Q.defer()
+  const room = global.conn.collection('Sala')
+  // validation
+  room.findOne(
+    { roomName: salaParam.roomName },
+    function (err, person) {
+      if (err) deferred.reject(err.name + ': ' + err.message)
 
-function create(salaParam) {
-    var deferred = Q.defer();
-    var room = global.conn.collection("Sala");
-    // validation
-    room.findOne(
-        { roomName: salaParam.roomName },
-        function (err, person) {
-            if (err) deferred.reject(err.name + ': ' + err.message);
+      if (person) {
+        // room already exists
+        deferred.reject('RoomName "' + salaParam.roomName + '" is already taken')
+      } else {
+        createRoom()
+      }
+    })
 
-            if (person) {
-                // room already exists
-                deferred.reject('RoomName "' + salaParam.roomName + '" is already taken');
-            } else {
-                createRoom();
-            }
-        });
+  function createRoom () {
+    room.insertOne(
+      salaParam,
+      function (err, doc) {
+        if (err) deferred.reject(err.name + ': ' + err.message)
 
-    function createRoom() {
-        room.insertOne(
-            salaParam,
-            function (err, doc) {
-                if (err) deferred.reject(err.name + ': ' + err.message);
+        deferred.resolve()
+      })
+  }
 
-                deferred.resolve();
-            });
-    }
-
-    return deferred.promise;
+  return deferred.promise
 }
 
-function verifica_sala_ativa(roomname){
+function verifica_sala_ativa (roomname) {
+  const deferred = Q.defer()
+  const room = global.conn.collection('Sala')
 
-    var deferred = Q.defer();
-    var room = global.conn.collection("Sala");
+  room.findOne(
+    { roomName: roomname },
+    function (err, room) {
+      if (err) deferred.reject(err.name + ': ' + err.message)
 
-    room.findOne(
-        { roomName: roomname },
-        function (err, room) {
-            if (err) deferred.reject(err.name + ': ' + err.message);
-
-            if (room) {
-                // room already exists
-                if(room.status == 0){
-                    deferred.resolve(false);
-                }else{
-                    deferred.resolve(true);
-                }
-               
-            } else {
-                deferred.reject('A sala' +roomname + 'não foi encontrada');
-            }
-        });
-    
-    return deferred.promise;
-
-}
-
-function valida_codigo(codigo){
-
-    var deferred = Q.defer();
-    var room = global.conn.collection("Sala");
-
-    room.findOne(
-        { cod_acesso: codigo },
-        function (err, room) {
-            if (err) deferred.reject(err.name + ': ' + err.message);
-
-            if (room) {
-                deferred.resolve(true);                
-               
-            } else {
-                deferred.resolve(false); 
-            }
-        });
-    
-    return deferred.promise;
-
-}
-
-function lista_salas() {
-
-    var deferred = Q.defer();
-    var room = global.conn.collection("Sala");
-
-    room.find().toArray(function (err, room) {
-        if (err) deferred.reject(err.name + ': ' + err.message);
-
-        if (room) {
-            // return user (without hashed password)
-            deferred.resolve(room);
+      if (room) {
+        // room already exists
+        if (room.status == 0) {
+          deferred.resolve(false)
         } else {
-            // user not found
-            deferred.resolve();
+          deferred.resolve(true)
         }
-    });
+      } else {
+        deferred.reject('A sala' + roomname + 'não foi encontrada')
+      }
+    })
 
-    return deferred.promise;
+  return deferred.promise
+}
+
+function valida_codigo (codigo) {
+  const deferred = Q.defer()
+  const room = global.conn.collection('Sala')
+
+  room.findOne(
+    { cod_acesso: codigo },
+    function (err, room) {
+      if (err) deferred.reject(err.name + ': ' + err.message)
+
+      if (room) {
+        deferred.resolve(true)
+      } else {
+        deferred.resolve(false)
+      }
+    })
+
+  return deferred.promise
+}
+
+function lista_salas () {
+  const deferred = Q.defer()
+  const room = global.conn.collection('Sala')
+
+  room.find().toArray(function (err, room) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+
+    if (room) {
+      // return user (without hashed password)
+      deferred.resolve(room)
+    } else {
+      // user not found
+      deferred.resolve()
+    }
+  })
+
+  return deferred.promise
+}
+
+function update (salaParam) {
+  const deferred = Q.defer()
+  const salas = global.conn.collection('Sala')
+  // validation
+  salas.findOne({ _id: new ObjectID.createFromHexString(salaParam._id) }, function (err, sala) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    if (sala) {
+      updateSala()
+    }
+  })
+  function updateSala () {
+    const set = lodash.omit(salaParam, '_id')
+
+    salas.updateOne(
+      { _id: new ObjectID.createFromHexString(salaParam._id) },
+      { $set: set },
+      function (err, doc) {
+        if (err) {
+          deferred.reject(err.name + ': ' + err.message)
+        }
+
+        deferred.resolve()
+      })
+  }
+
+  return deferred.promise
 }
